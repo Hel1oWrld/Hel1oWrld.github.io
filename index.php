70345a6b1f257a953fe7ac5fee552d84c51bc01e
Hel1oWrld <137197541+Hel1oWrld@users.noreply.github.com>
2023-06-20 19:24:37 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("Hello! I am an alert box!");
+}
+</script>
+    <a href="https://twitter.com/elonmusk">Drag me</a>
+  </body>
+</html>
